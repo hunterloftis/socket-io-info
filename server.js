bfbadf90702244589bc99df3d1a6831aa4406f88
Hunter Loftis <hunter@hunterloftis.com>
2018-06-13 16:18:29 -0400

    deps

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 const path = require('path')
@@ -7,15 +8,16 @@ const path = require('path')
 const PORT = process.env.PORT || 3000
 const INDEX = path.join(__dirname, 'index.html')
 
-const server = express()
+const app = express()
   .use((req, res) => res.sendFile(INDEX))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const server = http.createServer(app)
 
 const io = socketIO(server)
+  .on('connection', (socket) => {
+    console.log('Client connected')
+    socket.emit('name', process.env.DYNO || 'unknown')
+    socket.on('disconnect', () => console.log('Client disconnected'))
+  })
 
-io.on('connection', (socket) => {
-  console.log('Client connected')
-  socket.emit('name', process.env.DYNO || 'unknown')
-  socket.on('disconnect', () => console.log('Client disconnected'))
-})
-
+server.listen(PORT, () => console.log(`Listening on ${PORT}`))
